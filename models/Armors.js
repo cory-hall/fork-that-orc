e5e816e1c1d158e8ffa9d87f473f0d192b55ffbc
auscarter17 <98193003+auscarter17@users.noreply.github.com>
2022-05-10 19:50:27 -0400

    created armor model with parameters

--- models/Armors.js
+++ models/Armors.js
@@ -1,2 +1,21 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Armor extends Model {}
+
+Armor.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        armor_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }
+)
+
+module.exports = Armor;
