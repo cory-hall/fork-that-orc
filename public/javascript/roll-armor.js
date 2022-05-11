65a4accd347263cb7f72f201e15d40e2007cc388
Cory Hall <ComebackCory1@gmail.com>
2022-05-10 19:28:02 -0500

    added roll armor file

--- public/javascript/roll-armor.js
+++ public/javascript/roll-armor.js
@@ -1,4 +1,19 @@
-const rand = require('../../utils/random-number')
+const randomInt = require('../../utils/random-number')
+const { Armor } = require('../../models');
 
+function findArmor() {
+  const select = document.querySelector('#class-select');
+  const text = select.options[select.selectedIndex].text;
 
+  Armor.findAll({
+    where: {
+      armor_class: text
+    }
+  })
+  .then(armorDbData => {
+    const armor = armorDbData.map(armor => armor.get({ plain : true }));
+    const index = randomInt(armor);
 
+    return index;
+  })
+}
