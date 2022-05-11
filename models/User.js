91d1ddc7fe52f7a1f86608b93168ea35151aaa41
JoelHauser <joel.hauser@gmail.com>
2022-05-11 01:00:18 -0400

    kermitdefroghere

--- models/User.js
+++ models/User.js
@@ -47,7 +47,7 @@ class User extends Model {
       timestamps: false,
       freezeTableName: true,
       underscored: true,
-      modelName: 'user'
+      modelName: 'User'
     }
   );
   
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 const seedWeapons = require('./weapon-seeds');
 const seedArmors = require('./armor-seeds');
 
+
 // const seedConsumables = require('./consumable-seeds');
 
 const sequelize = require('../config/connection');
@@ -14,6 +15,9 @@ const seedAll = async () => {
 
     await seedArmors();
     console.log('--------------');
+
+    await seedUser();
+    console.log('8============D');
 
     // await seedConsumables();
     // console.log('--------------');
