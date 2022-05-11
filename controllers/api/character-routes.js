03aabc6a6f21d1e3f957cf37eaac6f914d45dc3a
TrevorTress <trevor.tress@outlook.com>
2022-05-10 20:00:35 -0400

    character routes updated

--- controllers/api/character-routes.js
+++ controllers/api/character-routes.js
@@ -5,8 +5,76 @@ const { Armor, Character, Consumables, Inventory, User, Weapons } = require('../
 module.exports = router;
 
 
-// get all character from user
+
+
+
+// create character
+router.post('/', (req, res) => {
+    Character.create({
+
+    })
+    .then(userData => {
+        res.json(userData)
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    })
+})
+
+// get all characters from user
+router.get('/', (req, res) => {
+    Character.findAll({
+
+    })
+    .then(userData => {
+        res.json(userData)
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    })
+})
 
 // get one character from user
+router.get('/:id', (req, res) => {
+    Character.findOne({
+
+    })
+    .then(userData => {
+        res.json(userData)
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    })
+})
+
+// update character trait
+router.put('/', (req, res) => {
+    Character.update({
+
+    })
+    .then(userData => {
+        res.json(userData)
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    })
+})
 
 // delete character
+router.delete('/:id', (req, res) => {
+    Character.destroy({
+
+    })
+    .then(userData => {
+        res.json(userData)
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    })
+})
+
--- controllers/api/inventory-routes.js
+++ controllers/api/inventory-routes.js
@@ -1,5 +1,5 @@
-const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const { Armor, Character, Consumables, Inventory, User, Weapons } = require('../../models');
+// const router = require('express').Router();
+// const sequelize = require('../../config/connection');
+// const { Armor, Character, Consumables, Inventory, User, Weapons } = require('../../models');
 
-module.exports = router;
+// module.exports = router;
