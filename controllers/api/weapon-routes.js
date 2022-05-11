0e6814d2247b22f6b54197dfd888c6e0452ca90d
Cory Hall <ComebackCory1@gmail.com>
2022-05-10 21:31:57 -0500

    added weapon get routes

---
+++ controllers/api/weapon-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Weapons } = require('../../models/')
+
+// GET all weapons
+router.get('/', (req, res) => {
+  Weapons.findAll({})
+    .then(dbData => res.json(dbData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// GET one weapon by `id` value
+router.get('/:id', (req, res) => {
+  Weapons.findAll({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbData => res.json(dbData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST to create a new weapon
+router.post('/', (req, res) => {
+  if (req.session) {
+    // expects {"weapon_name": "Sword", "weapon_class": "Warrior", "weapon_rating": "8"
+    Weapons.create({
+      weapon_name: req.body.weapon_name,
+      weapon_class: req.body.weapon_class,
+      weapon_rating: req.body.weapon_rating
+    })
+  }
+})
