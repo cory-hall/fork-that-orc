fee1e526773c72c69c0c8f36e869361bc18dffd0
Cory Hall <ComebackCory1@gmail.com>
2022-05-15 21:20:14 -0500

    adding roll

--- public/javascript/char-page.js
+++ public/javascript/char-page.js
@@ -1,3 +1,5 @@
+var Roll = require('roll'),
+  roll = new Roll();
 
 // global name arrays
 const firstName = [
@@ -179,25 +181,25 @@ const rollStatHandler = (event) => {
 
   switch (charClass) {
     case 'warrior':
-      var health = randomInt(100) + 50;
-      var mana = randomInt(100);
-      var str = randomInt(10) + 10;
-      var dex = randomInt(10) + 5;
-      var int = randomInt(10);
+      var health = roll.roll('d%') + 50;
+      var mana = roll.roll('d%');
+      var str = roll.roll('2d6') + 10;
+      var dex = roll.roll('2d6') + 5;
+      var int = roll.roll('2d6');
       break;
     case 'rogue':
-      var health = randomInt(100) + 25;
-      var mana = randomInt(100) + 25;
-      var str = randomInt(10) + 5;
-      var dex = randomInt(10) + 5;
-      var int = randomInt(10) + 5;
+      var health = roll.roll('d%') + 25;
+      var mana = roll.roll('d%') + 25;
+      var str = roll.roll('2d6') + 5;
+      var dex = roll.roll('2d6') + 5;
+      var int = roll.roll('2d6') + 5;
       break;
     case 'mage':
-      var health = randomInt(100);
-      var mana = randomInt(100) + 50;
-      var str = randomInt(10);
-      var dex = randomInt(10) + 5;
-      var int = randomInt(10) + 10;
+      var health = roll.roll('d%');
+      var mana = roll.roll('d%') + 50;
+      var str = roll.roll('2d6');
+      var dex = roll.roll('2d6') + 5;
+      var int = roll.roll('2d6') + 10;
       break;
     default:
       break;
