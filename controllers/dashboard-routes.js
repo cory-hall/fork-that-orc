cfee730f5fde79b285f5b715a67ebb38460edd54
Cory Hall <ComebackCory1@gmail.com>
2022-05-16 01:57:22 -0500

    fixing login page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,24 +2,23 @@ const router = require('express').Router();
 const { User, Weapons, Armors, Character } = require('../models');
 const withAuth = require('../utils/auth');
 
+// router.get('/', (req, res) => {
+//   res.render('dashboard');
+// })
+
+
 // GET all builds for dashboard view, with withAuth helper
-router.get('/heros', withAuth, (req, res) => {
+router.get('/', withAuth, (req, res) => {
   Character.findAll({
     where: {
        // get user_id from session user_id
     user_id: req.session.user_id
-    },
-    include: [
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
+    }
   })
     .then(dbData => {
       // serialize the data before passing to template
       const builds = dbData.map(build => build.get({ plain: true }));
-      res.render('heros', { builds, loggedIn: true });
+      res.render('user-heros', { builds, loggedIn: true });
     })
     .catch(err => {
       console.log(err);
