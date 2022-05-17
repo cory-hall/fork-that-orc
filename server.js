7ddadb4ac5b68a6ae450be4d95c5a6ff019a5641
TrevorTress <trevor.tress@outlook.com>
2022-05-17 19:37:49 -0400

    hero icons conditionally generated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 // init express
 const app = express();
